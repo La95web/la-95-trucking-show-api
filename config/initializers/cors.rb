ebe18a023cbdd273f14861656f00a0c932dda3ed
rpire <rubenpire7@gmail.com>
2025-01-25 03:53:09 -0400

    Install and configure Rack CORS
    Install and configure the Rack CORS gem for handling Cross-Origin Resourses sharing, making cross-origin Ajax possible.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,17 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins "localhost:5500"
+
+    resource "/v1/users/log_in",
+      headers: :any,
+      methods: %i[post],
+      expose: [ "Authorization" ]
+
+    resource "*",
+      headers: :any,
+      methods: :any
+  end
+end
