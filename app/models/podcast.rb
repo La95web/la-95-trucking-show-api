1675262aaaa4e3f09c5204e85689defc336838de
rpire <rubenpire7@gmail.com>
2025-02-04 21:55:29 -0400

    Add model validations to Podcast

--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -1,3 +1,8 @@
 class Podcast < ApplicationRecord
   has_one_attached :thumbnail
+
+  validates :title, presence: true
+  validates :title, lenght: { in: 5..100 }
+  validates :video_url, presence: true
+  validates :uploaded_at, presence: true
 end
