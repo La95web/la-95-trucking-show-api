271f5eee6c9ed0b76a9f58ffa4d74deca77e556d
rpire <rubenpire7@gmail.com>
2025-02-05 22:41:34 -0400

    Generate the V1::Articles controller

---
+++ app/controllers/v1/articles_controller.rb
@@ -0,0 +1,34 @@
+class V1::ArticlesController < ApplicationController
+  load_and_authorize_resource
+
+  def index; end
+
+  def show; end
+
+  def create
+    if @article.save
+      render status: :created
+    else
+      render json: @article.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @article.update(article_params)
+      head :no_content
+    else
+      render json: @article.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article.destroy
+    head :no_content
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content, :type, :main_image, :thumbnail)
+  end
+end
