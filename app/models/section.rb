8958f949fcce3ad8a193ad57e2d6b2619f7d74f1
rpire <rubenpire7@gmail.com>
2025-02-05 23:06:09 -0400

    Add model validations to Section

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,4 +1,7 @@
 class Section < ApplicationRecord
   belongs_to :article
   has_one_attached :image
+
+  validates :heading, presence: true, length: { in: 5..100 }
+  validates :body, presence: true, length: { in: 5..1000 }
 end
