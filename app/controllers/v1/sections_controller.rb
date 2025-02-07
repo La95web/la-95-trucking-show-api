a1cb713a175ea354fa7332e4eec31c6bc8fa4110
rpire <rubenpire7@gmail.com>
2025-02-07 00:52:40 -0400

    Generate the Sections controller

---
+++ app/controllers/v1/sections_controller.rb
@@ -0,0 +1,33 @@
+class V1::SectionsController < ApplicationController
+  load_and_authorize_resource :article
+  load_and_authorize_resource :section, through: :article
+
+  def show; end
+
+  def create
+    if @section.save
+      render status: :created
+    else
+      render json: @section.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @section.update(section_params)
+      head :no_content
+    else
+      render json: @section.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @section.destroy
+    head :no_content
+  end
+
+  private
+
+  def section_params
+    params.require(:section).permit(:title, :body, :type, :main_image, :thumbnail)
+  end
+end
