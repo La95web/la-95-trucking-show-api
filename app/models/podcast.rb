73eeb5a4dd3a6ed4a7bfee8ef02973be0587ce2c
rpire <rubenpire7@gmail.com>
2025-02-07 01:15:09 -0400

    Fix typo in the Podcast model

--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -2,7 +2,7 @@ class Podcast < ApplicationRecord
   has_one_attached :thumbnail
 
   validates :title, presence: true
-  validates :title, lenght: { in: 5..100 }
+  validates :title, length: { in: 5..100 }
   validates :video_url, presence: true
   validates :uploaded_at, presence: true
 end
