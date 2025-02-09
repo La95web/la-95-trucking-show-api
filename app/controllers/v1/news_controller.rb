985ab326793a3fb73ee52d748e8cda039e3f1862
rpire <rubenpire7@gmail.com>
2025-02-09 00:28:04 -0400

    Generate the News controller

---
+++ app/controllers/v1/news_controller.rb
@@ -0,0 +1,33 @@
+class V1::NewsController < ApplicationController
+  load_and_authorize_resource
+
+  def index; end
+
+  def show; end
+
+  def create
+    if @news.save
+      render :show, status: :created
+    else
+      render json: @news.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @news.update(news_params)
+      head :no_content
+    else
+      render json: @news.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @news.destroy
+  end
+
+  private
+
+  def news_params
+    params.require(:news).permit(:title, :type, :uploaded_at, :main_image, :thumbnail)
+  end
+end
