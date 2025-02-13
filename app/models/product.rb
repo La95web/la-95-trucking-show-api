aee84f89b0e4ff4edd653e8f94b2ed25f00d59b2
rpire <rubenpire7@gmail.com>
2025-02-13 03:28:42 -0400

    Add model validations for Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
   has_one_attached :main_image
   has_many_attached :gallery
+
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :description, presence: true, length: { maximum: 500 }
+  validates :price, presence: true, numericality: { in: 0...100_000 }
+  validates :stripe_id, presence: true, uniqueness: true
 end
