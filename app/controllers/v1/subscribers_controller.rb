a7af5f5ae18c1ef6072c2e9a3b42636e76a3db36
rpire <rubenpire7@gmail.com>
2025-02-18 16:49:45 -0400

    Generate the Subscribers controller and methods

---
+++ app/controllers/v1/subscribers_controller.rb
@@ -0,0 +1,29 @@
+class V1::SubscribersController < ApplicationController
+  def index
+    @subscribers = Subscriber.all
+    render json: @subscribers, status: :ok
+  end
+
+  def create
+    @subscriber = Subscriber.new(subscriber_params)
+
+    if @subscriber.save
+      render json: @subscriber, status: :created
+    else
+      render json: { error: @subscriber.errors.full_messages.first }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    authorize! :destroy, @subscriber
+    @subscriber = Subscriber.find(params[:id])
+    @subscriber.destroy
+    head :no_content
+  end
+
+  private
+
+  def subscriber_params
+    params.require(:subscriber).permit(:email)
+  end
+end
