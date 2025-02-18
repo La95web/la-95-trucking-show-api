d0ba4cbd80aa3e27a6347473486fa961749f18f8
rpire <rubenpire7@gmail.com>
2025-02-18 16:49:17 -0400

    Generate the Subscriber model and validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_18_194607) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_18_202717) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -87,6 +87,12 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_18_194607) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_sections_on_article_id"
+  end
+
+  create_table "subscribers", force: :cascade do |t|
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
