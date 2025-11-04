10555bda0d83ad66011dbf52a26b27cca67dfc0d
louis17782 <luissanteliz22@gmail.com>
2025-11-04 17:30:55 -0400

    add new migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_21_054842) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_04_174618) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -58,6 +58,16 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_21_054842) do
     t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "marketplaces", force: :cascade do |t|
+    t.string "title"
+    t.date "uploaded_at"
+    t.text "content"
+    t.integer "language"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "paragraphs"
   end
 
   create_table "podcasts", force: :cascade do |t|
