b17c48b095b24ff51cea6b7bf654b737e31a7f82
louis17782 <luissanteliz22@gmail.com>
2025-11-11 15:43:25 -0400

    add migration to audios

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_04_174618) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_10_172754) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -50,6 +50,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_04_174618) do
     t.datetime "updated_at", null: false
     t.text "content"
     t.integer "language", default: 0
+  end
+
+  create_table "audios", force: :cascade do |t|
+    t.string "title"
+    t.date "uploaded_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "contact_submissions", force: :cascade do |t|
