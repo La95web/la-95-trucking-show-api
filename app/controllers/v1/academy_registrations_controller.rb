c5e037c63f2cfcdae95ab9b6d951e69dcf18bc11
louis17782 <luissanteliz22@gmail.com>
2026-07-30 21:51:20 -0400

    delete some comment line

--- app/controllers/v1/academy_registrations_controller.rb
+++ app/controllers/v1/academy_registrations_controller.rb
@@ -6,12 +6,10 @@ class V1::AcademyRegistrationsController < ApplicationController
     @academy_registration = AcademyRegistration.new(academy_registration_params)
 
     if @academy_registration.save
-      # Disparamos el correo de notificación
       AcademyMailer.new_registration_email(@academy_registration).deliver_now
 
       render json: @academy_registration, status: :created
     else
-      # Respondemos con el error en el mismo formato que tu otro controlador
       render json: { error: @academy_registration.errors.full_messages.first }, status: :unprocessable_entity
     end
   end
@@ -19,7 +17,6 @@ class V1::AcademyRegistrationsController < ApplicationController
   private
 
   def academy_registration_params
-    # Permitimos los parámetros usando los nombres de las columnas de la base de datos
     params.require(:academy_registration).permit(:full_name, :email, :phone, :has_cdl, :driving_experience)
   end
 end
